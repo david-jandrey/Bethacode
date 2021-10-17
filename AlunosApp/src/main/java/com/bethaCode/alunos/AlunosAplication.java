c6c832db33eda97b129451ae878b558b68f38e9a
David Jandey <56413390+david-jandrey@users.noreply.github.com>
2021-10-17 16:17:20 -0300

    Add files via upload

---
+++ AlunosApp/src/main/java/com/bethaCode/alunos/AlunosAplication.java
@@ -0,0 +1,12 @@
+package com.bethaCode.alunos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlunosAplication {
+    public static  void  main(String[] args){
+        SpringApplication.run(AlunosAplication.class, args);
+    }
+
+}
